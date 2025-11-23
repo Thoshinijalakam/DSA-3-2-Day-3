c450fd999a0c1012746ee41f9793e1cd8623f1c1
Thoshinijalakam <thoshinijalakam@gmail.com>
2025-11-23 11:40:47 +0530

    Recursion-base code+recursion code

---
+++ recursion_sum.java
@@ -0,0 +1,13 @@
+public class recursion_sum{
+    public static void main(String[] args) {
+        System.out.println(add(5));
+    }
+        public static int add(int num){
+            if(num==0){
+                return 0;
+            }else{
+                return num+add(num-1);
+            }
+        }
+}  
+ 
