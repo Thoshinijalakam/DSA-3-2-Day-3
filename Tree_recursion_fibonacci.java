3c4b5bd8f9ec1e81b45346f7c95f1f31fa3834d0
Thoshinijalakam <thoshinijalakam@gmail.com>
2025-11-24 09:47:20 +0530

    fibonacci series using Tree recursion

---
+++ Tree_recursion_fibonacci.java
@@ -0,0 +1,11 @@
+public class Tree_recursion_fibonacci{
+    public static void main(String[] args) {
+        System.out.println(fibonacci(6));
+    }
+    public static int fibonacci(int num){
+        if(num<=1){
+            return num;
+        }
+        return fibonacci(num-1)+fibonacci(num-2);
+    }
+}
