e9f8af62f6834c653e8ebeb1a557ed948263b4d9
Thoshinijalakam <thoshinijalakam@gmail.com>
2025-11-24 09:44:05 +0530

    to find factorial of number using Tail recursion

---
+++ Tail_recursion_fact.java
@@ -0,0 +1,11 @@
+public class Tail_recursion_fact{
+    public static void main(String[] args) {
+        System.out.println(fact(5,1));
+    }
+    public static int fact(int n,int num){
+        if(n==1){
+            return num;
+        }
+        return fact(n-1,n*num);
+    }
+}
